d8659fc648e882d327f4a703966c23d7a7febeaa
Mark Hillebrand <mahilleb@microsoft.com>
2016-08-10 16:48:45 +0200

    reverse loop

--- Source/Readers/ReaderLib/PackerBase.cpp
+++ Source/Readers/ReaderLib/PackerBase.cpp
@@ -47,7 +47,8 @@ PackerBase::PackerBase(MemoryProviderPtr memoryProvider,
     m_streamBuffers.reserve(m_outputStreamDescriptions.size());
 
     // Sanity checks:
-    for (size_t i = 0; i < m_outputStreamDescriptions.size(); ++i)
+    size_t i = m_outputStreamDescriptions.size();
+    while (i--)
     {
         const auto& stream = m_outputStreamDescriptions[i];
         UNUSED(stream);
