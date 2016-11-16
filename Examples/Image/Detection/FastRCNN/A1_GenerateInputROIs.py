44527f540fbc3367cfc4c69cdc5bed553b35fbf8
Philipp Kranen <pkranen@microsoft.com>
2016-11-16 16:23:10 +0100

    reverting minor changes

--- Examples/Image/Detection/FastRCNN/A1_GenerateInputROIs.py
+++ Examples/Image/Detection/FastRCNN/A1_GenerateInputROIs.py
@@ -22,7 +22,6 @@ boAddRoisOnGrid = True
 # Main
 ####################################
 # generate ROIs using selective search and grid (for pascal we use the precomputed ROIs from Ross)
-#if False: # not datasetName.startswith("pascalVoc"):
 if not datasetName.startswith("pascalVoc"):
     # init
     makeDirectory(roiDir)
@@ -110,9 +109,6 @@ for image_set in image_sets:
                 currGtOverlaps = imdb.roidb[imgIndex]['gt_overlaps']
                 imgPath = imdb.image_path_at(imgIndex)
                 imgWidth, imgHeight = imWidthHeight(imgPath)
-
-                #import pdb
-                #pdb.set_trace()
 
                 # all rois need to be scaled + padded to cntk input image size
                 targetw, targeth, w_offset, h_offset, scale = roiTransformPadScaleParams(imgWidth, imgHeight,
